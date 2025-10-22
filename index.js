fefc2e41280c45af09e0219086c38cf54e3a75df
sekarasriii <sekareeesh@gmail.com>
2025-10-22 11:19:22 +0700

    inisialisasi server express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
